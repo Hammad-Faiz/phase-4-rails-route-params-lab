81ab53330528d7806896b64e40892f7a366437c9
Hammad <hammadfaiz172@gmail.com>
2022-08-28 23:47:17 -0400

    commit tree bd5bcfbd8b8dd8b92b4bb5921912100eaed9c072

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,19 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def first_student
+    student = Student.first
+    render json: Student
+  end
+
+  def second_student
+    student = Student.second
+    render json: student
+  end
+
+  def students
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 end
